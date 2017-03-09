91c09924b6d7aa789db8cf34c83a6e507b9b284f
lxndrvn <i1990522@gmail.com>
2017-03-09 20:54:31 +0100

    add methods

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+class ContactList(list):
+    def search(self, name):
+        matching_contacts = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts
 
-class Contact:
-    pass
+    def longest_name(self):
+        longest = ""
+        for contact in self:
+            if len(contact.name) > len(longest):
+                longest = contact.name
+                if longest:
+                    return longest
+
+
+class Contact(object):
+    all_contacts = ContactList()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        return cls.all_contacts.clear()
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    def order(self, order):
+        key = self.email
+        value = [order]
+        if key in self.all_orders:
+            self.all_orders[key].append(value)
+        else:
+            self.all_orders[key] = value
